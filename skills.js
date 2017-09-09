661aff30c32d648b027dd75387f06241358fe763
Andrew Ding <andrewxding@personalmicrosoftsoftware.ucla.edu>
2017-09-09 14:12:59 -0700

    kind of janky on really small and mobile

--- skills.js
+++ skills.js
@@ -81,7 +81,7 @@ var o = {
 		return Math.floor((Math.random()*(u-l+1))+l);
 	},
 	diagram: function(){
-		var r = Raphael('diagram', 600, 600),
+		var r = Raphael('diagram', 550, 550),
 			rad = 73;
 
 		r.circle(300, 300, 85).attr({ stroke: 'none', fill: '#193340' });
@@ -112,7 +112,7 @@ var o = {
 				text = t.find('.text').text();
 
 			rad += 30;	
-			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': 26 });
+			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': 20 });
 			var clicked = false;
 			z.mouseover(function(){
 				
